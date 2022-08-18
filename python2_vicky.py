7e22bd21efb94f900c42025e7f8857c8b0d2b678
VICKYFTWANU <111479830+VICKYFTWANU@users.noreply.github.com>
2022-08-18 00:01:40 -0700

    Add files via upload

---
+++ python2_vicky.py
@@ -0,0 +1,8 @@
+nama_depan ="vicky"
+nama_tengah ="julio"
+nama_belakang ="borses"
+nama_lengkap ="vicky julio borses"
+print("nama depan saya adalah",nama_depan)
+print("nama tengah saya adalah",nama_tengah)
+print("nama nama belakang saya adalah",nama_belakang)
+print(" nama lengkap saya adalah ",nama_depan,nama_tengah,nama_belakang)
