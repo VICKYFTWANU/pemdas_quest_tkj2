89f7c1d51ee5713e158980df89f204b993a87a54
VICKYFTWANU <111479830+VICKYFTWANU@users.noreply.github.com>
2022-08-21 22:10:57 -0700

    Add files via upload

---
+++ python_latihan.py
@@ -0,0 +1,11 @@
+print("hallo semuanya")
+nama_saya_adalah = input ("\nnama saya adalah")
+print("hari ini kita akan belajar rotasi hyper ya")
+hero = input("\nnama hero")
+print("pertama-tama farming")
+print("setelah itu kita roam ke gold lane usahakan dapat kill")
+print("lalu langsung set up turtle")
+item = input("\nitem awal adalah")
+
+print("oke sampai hari ini saja pelajaran hari ini")
+print("terimakasih")
