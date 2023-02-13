35f002306054c30b7664eebae5abfced36e39feb
VICKYFTWANU <111479830+VICKYFTWANU@users.noreply.github.com>
2023-02-13 10:02:20 +0700

    Add files via upload

---
+++ vicky-tkj2/lthn-fungsi-vicky.py
@@ -0,0 +1,49 @@
+'''def nama_fungsi():'''
+
+def salam():
+    print("OII OI OI NANDEO?")
+
+salam()
+
+
+
+'''def nama_fungsi():'''
+
+def TOKYO_RAVENGER():
+    print("YAMATE KUDASAIIIII IAHHHH,ONEE CHAN AHH AHH AH")
+
+TOKYO_RAVENGER()
+TOKYO_RAVENGER()
+
+
+def perkenalkan(nick,role,rank):
+    print("nama saya:",nick)
+    print("role saya:",role)
+    print("rank saya:",rank)
+
+perkenalkan("Flourescent.","assasin","mythic")
+
+
+
+def perkenalkan(nama,alamat,umur):
+    print("nama saya:",nama)
+    print("alamat saya:",alamat)
+    print("umur saya:",umur)
+
+perkenalkan("vicky","depok","gada yg tau")
+
+def luas_segitiga():
+    print('mencari luas segitiga')
+    x=float(input('masukan alas segitiga:'))
+    y=float(input('masukan tinggi segitiga:'))
+    a = 0.5*y*x
+
+print('luas segitiga adalah:','60cm')
+
+def luas_pp():
+    print('mencari luas persegi panjang')
+    x=float(input('masukan panjangnya:'))
+    y=float(input('masukan lebarnya:'))
+    c = x*y
+
+print('luasa persegi panjang adalah:','10cm')
